4afd1216978bcf52e9bbc293e6452cb49fcd6e28
Damir Iunusov <damir61205@gmail.com>
2024-11-05 20:18:41 +0300

    fixed 7th task

--- 7.cpp
+++ 7.cpp
@@ -6,27 +6,33 @@ int main()
 	int n;
 	cout << "Введите целое число n:";
 	cin >> n;
-	double count9{}, count8{}, count6{}, sum{};
+	double count9{}, count8{}, count6{}, sum{}, sum89{};
 	for (int i{}; i < n; i++)
 	{
 		int number;
 		cout << "Введите целое число:";
 		cin >> number;
-		if (abs(number) % 10 == 9)
-			count9++;
-		else if (abs(number) % 10 == 8)
-			count8++;
-		if (abs(number) % 6 == 0)
-			count6++;
 		int numb = abs(number);
 		int count{};
+		if (numb % 10 == 9)
+		{
+			count9++;
+			sum89 += number;
+		}
+		else if (numb % 10 == 8)
+		{
+			count8++;
+			sum89 += number;
+		}
+		if (number % 6 == 0)
+			count6++;
 		while (numb > 10)
 			numb /= 10;
 		if (numb == 3)
 			sum += number;
 	}
-	if (count9 > 0 || count8 > 0)
-		cout << "Среднее арифметическое чисел, оканчивающихся на 9 или 8 = " << (count9 * 9 + count8 * 8) / (count9 + count8) << endl;
+	if (count9 + count8 > 0)
+		cout << "Среднее арифметическое чисел, оканчивающихся на 9 или 8 = " << sum89 / (count9 + count8) << endl;
 	else
 		cout << "Среднее арифметическое чисел, оканчивающихся на 9 или 8 не может быть вычислено, т.к. таких чисел нет \n";
 	cout << "Общее количество чисел, кратных 6 = " << count6 << endl;
